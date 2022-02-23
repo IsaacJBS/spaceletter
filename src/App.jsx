a3e82694462a366aa1387e21f8e6743357d6b653
Isaac <isaac.jordaobs@gmail.com>
2022-02-23 14:33:35 -0300

    fix: export arrow function no useData

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { ToastContainer } from 'react-toastify';
+import ParticlesBackground from "./components/ParticlesBackground";
+import './index.css';
+import DataProvider from "./contexts/DataProvider";
+import RoutesApp from "./Routes";
+
 function App() {
   return (
     <div className="App">
-      <h1>Oi</h1>
-    </div >
-  );
+      <ParticlesBackground />
+      <DataProvider>
+        <RoutesApp />
+      </DataProvider>
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App;
--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { useContext } from "react";
 import { ContextData } from '../contexts/DataProvider';
 
