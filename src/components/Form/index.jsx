47425db5440981c470a8f37882eb79ffbfd60dd8
Isaac <isaac.jordaobs@gmail.com>
2022-02-23 14:27:40 -0300

    adicionando componentes

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import React from 'react';
+
+function Form({ children }) {
+    return (
+        <main className='form'>{children}</main>
+    )
+}
+
+export default Form;
---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,58 @@
+import Particles from 'react-tsparticles';
+
+function ParticlesBackground() {
+    return (
+        <div>
+            <Particles
+                options={{
+                    background: {
+                        color: 'black',
+                    },
+                    fpsLimit: 60,
+                    interactivity: {
+                        detectsOn: 'canvas',
+                        events: {
+                            resize: true,
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: '#FFFFFF'
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 1080
+                            },
+                            limit: 0,
+                            value: 400,
+                        },
+                        opacity: {
+                            animation: {
+                                enable: true,
+                                minimumValue: 0.5,
+                                speed: 1,
+                                sync: false
+                            },
+                            random: {
+                                enable: true,
+                                minimumValue: 1,
+                            }, value: 1
+                        },
+                        shape: {
+                            type: 'circle',
+                        },
+                        size: {
+                            random: {
+                                enable: true,
+                                minimumValue: 2,
+                            },
+                            value: 1
+                        }
+                    }
+                }} />
+        </div>
+    );
+};
+
+export default ParticlesBackground;
