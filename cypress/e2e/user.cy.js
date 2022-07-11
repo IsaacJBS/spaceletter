59209f4d8d0f7a4eb114a98048cc77942f4c595e
Isaac <isaac.jordaobs@gmail.com>
2022-07-11 16:31:02 -0300

    test: teste do usuário completo

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,3 +1,5 @@
+const userEmail = "d@gmail.com"
+
 describe('Subscription in spaceletter', () => {
   it('should return an error because form is empty', () => {
     cy.visit('/')
@@ -8,14 +10,32 @@ describe('Subscription in spaceletter', () => {
   })
 
   it('should pass because email was informed', () => {
-    cy.get('form').click().type("d@gmail.com").submit()
+    cy.get('form').click().type(userEmail).submit()
 
     cy.contains("Cadastrado com sucesso")
   });
 
   it('should not pass because email has already been registered', () => {
-    cy.get('form').click().submit()
+    cy.get('form').click().type(userEmail).submit()
 
     cy.contains("Email já cadastrado")
+  });
+})
+
+describe("Unsubscription flow", () => {
+  it('should go to unsub page', () => {
+    cy.visit("/unsub");
+  });
+
+  it('should return an error because input is empty', () => {
+    cy.get('form').submit()
+
+    cy.contains("Por favor, insira um e-mail")
+  });
+
+  it('unsubscribe the user', () => {
+    cy.get('form').type(userEmail).submit()
+
+    cy.contains("O sinal da sua base foi desligado")
   });
 })
